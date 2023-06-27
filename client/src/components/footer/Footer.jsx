622ab0aad7bcebebe362f43030f73bb3d27ec8d4
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-06-27 15:58:45 -0500

    added footer for development porpuses

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import './Footer.css';
+
+export function Footer() {
+	// const { filters } = useFilters()
+
+	return (
+		<footer className="footer">
+			<div>
+				<img src="shop-logo.png" alt="Shopping cart icon" />
+				<h4>E-commerce unificado </h4>
+			</div>
+
+			<h5>
+				<span>Jhon Acosta</span>
+			</h5>
+		</footer>
+	);
+}
