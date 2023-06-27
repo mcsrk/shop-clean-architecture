f18042d4bbb3abe0c43874f1eaccadd82fb1affa
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-06-27 13:52:24 -0500

    initialized client

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1>Shop initialized with vite</h1>
+			<div className="card">
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</div>
+		</>
+	);
+}
+
+export default App;
