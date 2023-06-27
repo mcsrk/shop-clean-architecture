abac92f43727d243f759911ad571d67340caa4a0
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-06-27 15:59:32 -0500

    structured app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css';
+import { products as initialProducts } from './mocks/products.json';
+
+// Config
+import { IS_DEVELOPMENT } from './config.js';
+
+// Hooks
+import { useFilters } from './hooks/useFilters.js';
+
+// Components
+import { Header } from './components/header/Header';
+import { Products } from './components/content/Products';
+import { Footer } from './components/footer/Footer.jsx';
 
 function App() {
+	const { filterProducts } = useFilters();
+
+	const filteredProducts = filterProducts(initialProducts);
 	return (
 		<>
-			<h1>Shop initialized with vite</h1>
-			<div className="card">
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
+			<Header />
+			<main>
+				<Products products={filteredProducts} />
+			</main>
+			{IS_DEVELOPMENT && <Footer />}
 		</>
 	);
 }
