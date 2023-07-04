924a2012e4ea353ca0e20289ccdcde07dea00aca
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-07-03 22:28:53 -0500

    removed useless import

--- server/src/entities/ecommerce/ecommerce.repository.interface.ts
+++ server/src/entities/ecommerce/ecommerce.repository.interface.ts
@@ -1,5 +1,4 @@
 import { IProduct } from '../product.interface';
-import { ExternalProductEntity } from './ecommerce.product.interface';
 
 export interface IExternalProductRepository {
 	searchProducts(companyPrefix: string, searchTerm: string): Promise<IProduct[][]>;
