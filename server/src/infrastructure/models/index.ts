1f9df0aec4baba13d9db79d93acbb8932159e56d
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-07-03 22:28:36 -0500

    refactored sync models file at models/

---
+++ server/src/infrastructure/models/index.ts
@@ -0,0 +1,17 @@
+// Custom library
+import Logging from '../library/Logging';
+
+// Models
+import ProductModel from './product.model';
+
+export async function syncModels() {
+	try {
+		/** Sync all the models involved */
+		await ProductModel.sync({ alter: true });
+		ProductModel.associate();
+
+		console.clear();
+	} catch (error: any) {
+		Logging.error(error);
+	}
+}
